889a990d6b53c03eef0dff76f3eff6abfd6265ff
Kevin Yu <Kevin.Yu.2468@gmail.com>
2023-01-09 10:10:05 -0800

    Add bouncing name in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,7 +15,9 @@ const Navbar = ({ sections }: SectionProps) => {
   return (
     <div className="w-screen h-16 fixed flex justify-between items-center bg-black text-white z-10">
       <div className="text-xl md:text-3xl grow ml-12 hidden md:block">
-        <p className="drop-shadow-md ">Kevin Yu</p>
+        <a href="#landing">
+          <p className="hover:animate-bounce">Kevin Yu</p>
+        </a>
       </div>
       <div className="flex grow flex-nowrap text-lg md:text-2xl justify-evenly">
         {sections.map((sectionProp: SectionProp) => {
