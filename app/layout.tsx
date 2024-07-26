ba705b56c366053149e05cd777103b35df3b26e1
Kevin Yu <Kevin.Yu.2468@protonmail.com>
2024-07-26 13:33:09 -0700

    feat: add google analytics to website

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,27 @@
 import React from "react";
+import Head from "next/head"
 
 export default function RootLayout({ children }: any) {
   return (
     <html lang="en">
-      <head>
+      <Head>
+
+        {/* <!-- Google tag (gtag.js) --> */}
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}></script>
+        <script dangerouslySetInnerHTML={{__html: `
+          window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments)}
+            gtag('js', new Date());
+
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');`}}/>
+
         <meta name="description" content="Free Web tutorials" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#000000" />
         <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
 
         <title>Kevin Yu Portfolio</title>
-      </head>
+      </Head>
       <body>{children}</body>
     </html>
   );
